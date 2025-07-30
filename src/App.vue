ff9ccf09ead1f8d1aa320ef956ee01dd6f118b62
trungnd <trung.dn9500@gmail.com>
2025-07-30 23:34:52 +0700

    feat: test Notification component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
-import { useFcm } from 'fcm-rslib';
-import { onMounted, watch } from 'vue';
+import { Notification } from 'fcm-rslib';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyANUdxcxppINSZkayH4nGn4t-lOTg3B7N4',
-  authDomain: 'my-landing-page-e7c7e.firebaseapp.com',
-  projectId: 'my-landing-page-e7c7e',
-  storageBucket: 'my-landing-page-e7c7e.firebasestorage.app',
-  messagingSenderId: '999813447171',
-  appId: '1:999813447171:web:4d47bb99b3fa66909a9836',
-  measurementId: 'G-43J14KHN0W',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyANUdxcxppINSZkayH4nGn4t-lOTg3B7N4',
+//   authDomain: 'my-landing-page-e7c7e.firebaseapp.com',
+//   projectId: 'my-landing-page-e7c7e',
+//   storageBucket: 'my-landing-page-e7c7e.firebasestorage.app',
+//   messagingSenderId: '999813447171',
+//   appId: '1:999813447171:web:4d47bb99b3fa66909a9836',
+//   measurementId: 'G-43J14KHN0W',
+// };
 
-const { requestPermissionAndGetToken, initializeFCM, listNotification } = useFcm();
+// const { requestPermissionAndGetToken, initializeFCM, listNotification } = useNotification();
 
-watch(listNotification, (newVal) => {
-  console.log('[APP] listNotification:', newVal);
-});
+// watch(listNotification, (newVal) => {
+//   console.log('[APP] listNotification:', newVal);
+// });
 
-onMounted(() => {
-  requestPermissionAndGetToken();
-  initializeFCM();
-});
+// onMounted(() => {
+//   requestPermissionAndGetToken();
+//   initializeFCM();
+// });
 </script>
 
 <template>
-  <div>
+  <div :style="{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }">
+    <Notification />
   </div>
 </template>
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
+import 'fcm-rslib/dist/index.css';
 
 createApp(App).mount('#root');
