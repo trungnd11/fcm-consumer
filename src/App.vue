76cf1004769083ba55e43c232cc7c945c2b79752
trungnd <trung.dn9500@gmail.com>
2025-08-04 23:00:38 +0700

    feat: test init config

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <script setup lang="ts">
-import { Notification } from 'fcm-rslib';
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyANUdxcxppINSZkayH4nGn4t-lOTg3B7N4',
-//   authDomain: 'my-landing-page-e7c7e.firebaseapp.com',
-//   projectId: 'my-landing-page-e7c7e',
-//   storageBucket: 'my-landing-page-e7c7e.firebasestorage.app',
-//   messagingSenderId: '999813447171',
-//   appId: '1:999813447171:web:4d47bb99b3fa66909a9836',
-//   measurementId: 'G-43J14KHN0W',
-// };
-
-// const { requestPermissionAndGetToken, initializeFCM, listNotification } = useNotification();
-//
-// watch(listNotification, (newVal) => {
-//   console.log('[APP] listNotification:', newVal);
-// });
-//
-// onMounted(() => {
-//   requestPermissionAndGetToken();
-//   initializeFCM();
-// });
+import { Notification } from 'f-notification';
 </script>
 
 <template>
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
-import 'fcm-rslib/dist/index.css';
+import 'f-notification/dist/index.css';
 
 createApp(App).mount('#root');
